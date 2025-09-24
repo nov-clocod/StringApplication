7face9f6814b19b6f3fd7657acf13c99e4d58cd9
Sthealth <AW@example.com>
2025-09-24 13:57:42 -0400

    Added code to classes

--- src/main/java/com/pluralsight/Receipt.java
+++ src/main/java/com/pluralsight/Receipt.java
@@ -1,4 +1,11 @@
 package com.pluralsight;
 
 public class Receipt {
+    public static void main(String[] args) {
+        String itemName = "Cheese";
+        double itemPrice = 75.99;
+        int itemQuantity = 2;
+
+        System.out.println("You bought " + itemQuantity + " " + itemName + " " + "for $" + itemPrice );
+    }
 }
--- src/main/java/com/pluralsight/VariableApp.java
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -1,4 +1,27 @@
 package com.pluralsight;
 
 public class VariableApp {
+    public static void main(String[] args) {
+        String favColor = "Black";
+        short year = 2025;
+        char middle_initial = 'M';
+        boolean havePet = false;
+        String message = "You look awesome today";
+
+        System.out.println("My favorite color is: " + favColor);
+        System.out.println("The year I started this class is: " + year);
+        System.out.println("My middle middle_initial is: " + middle_initial);
+        System.out.println("I have a pet: " + havePet);
+        System.out.println("Nice message: " + message);
+
+        byte weekDays = 7;
+        String coffeePrice = "$4.99";
+        char favLetter = 'A';
+        boolean isRaining = false;
+
+        System.out.println("The number of days in a week is: " + weekDays);
+        System.out.println("The price of the coffee is: " + coffeePrice);
+        System.out.println("My favorite letter is: " + favLetter);
+        System.out.println("Is it raining today: " + isRaining);
+    }
 }
